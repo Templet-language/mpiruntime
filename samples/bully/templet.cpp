bc3b998d273d36132a2c87649c63475720e9d19c
devilseye <666devilseye666@gmail.com>
2016-05-31 15:53:18 +0400

    bully algorithm is finished

--- samples/bully/templet.cpp
+++ samples/bully/templet.cpp
@@ -4,20 +4,16 @@
 #include <math.h>
 #include <mpi.h>
 #include <windows.h>
+#include <cstdlib>
 
 using namespace std;
 
-#define CLOCK_ID	100
-#define ANSWER_ID	200
-#define VOTING_ID	300
-#define ERROR_ID	400
-#define COORD_ID	500
-
-typedef enum {
-    IDLE = 1000,
-    VOTING,
-    COORDINATING
-} State;
+#define CLOCK_ID		100
+#define ANSWER_ID		200
+#define VOTING_ID		300
+#define VOTING_OK_ID	350
+#define ERROR_ID		400
+#define COORD_ID		500
 
 struct engine; 
 struct proc; 
@@ -26,7 +22,6 @@ struct chan;
 struct proc{ 
 	void(*recv)(chan*, proc*); 
  	int id; // process ref in MPI message
-	State state;
 }; 
  
 struct chan{ 
@@ -79,54 +74,121 @@ inline bool access(chan*c, proc*p)
 
 void Process(chan*c, proc*p)
 {
+	bool coordinating = false;
+	bool voting = true;
+
 	int rank=saved_rank;
 	int size=saved_size;
-	while (1) {
-		if (rank == 0)
+
+	if (rank==size-1){
+		coordinating = true;
+	}
+
+	if (rank==0)
+	{
+		for (int i=1; i<size; i++)
 		{
-			for (int i=1; i<saved_size; i++)
-			{
-				MPI_Send(buffer, buff_size, MPI_INT, i, CLOCK_ID, MPI_COMM_WORLD);
-				cout << "[" << rank << "]: " << "Sending " << CLOCK_ID << " to [" << i << "]" << endl;
-			}
-		} else {
-			MPI_Status status;
-			MPI_Probe(0, MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-			MPI_Recv(buffer, buff_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			cout << "[" << rank << "]: " << "Received: " << status.MPI_TAG  << " from [" << status.MPI_SOURCE << "]" << endl;
-			switch (status.MPI_TAG) {
-				case CLOCK_ID:
-					if (rank==3)
-					{
-						MPI_Send(buffer, buff_size, MPI_INT, status.MPI_SOURCE, ERROR_ID, MPI_COMM_WORLD);
-						cout << "[" << rank << "]: " << "Sending " << ERROR_ID << " to [" << status.MPI_SOURCE << "]" << endl;
-					} else {
-						MPI_Send(buffer, buff_size, MPI_INT, status.MPI_SOURCE, ANSWER_ID, MPI_COMM_WORLD);
-						cout << "[" << rank << "]: " << "Sending " << ANSWER_ID << " to [" << status.MPI_SOURCE << "]" << endl;
-					}
-					break;
-			}
+			MPI_Send(buffer, buff_size, MPI_INT, i, CLOCK_ID, MPI_COMM_WORLD);
+			cout << "[" << rank << "]: " << "Sending [CLOCK] to [" << i << "]" << endl;
+			Sleep(400);
 		}
-		Sleep(1000);
-		if (rank == 0)
+	} else {
+		Sleep(400);
+		MPI_Status status;
+		MPI_Probe(0, MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+		MPI_Recv(buffer, buff_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		cout << "[" << rank << "]: " << "Received: " << ((status.MPI_TAG==CLOCK_ID) ? "[CLOCK]" : "[???]")  << " from [" << status.MPI_SOURCE << "]" << endl;
+		if (coordinating) {
+			coordinating = false;
+			MPI_Send(buffer, buff_size, MPI_INT, status.MPI_SOURCE, ERROR_ID, MPI_COMM_WORLD);
+			cout << "[" << rank << "]: " << "Sending [ERROR] to [" << status.MPI_SOURCE << "]" << endl;
+		} else {
+			MPI_Send(buffer, buff_size, MPI_INT, status.MPI_SOURCE, ANSWER_ID, MPI_COMM_WORLD);
+			cout << "[" << rank << "]: " << "Sending [ANSWER] to [" << status.MPI_SOURCE << "]" << endl;
+		}
+		Sleep(400);
+	}
+	if (rank==0)
+	{
+		Sleep(400);
+		for (int i=1; i<size; i++)
 		{
-			for (int i=1; i<saved_size; i++)
+			MPI_Status status;
+			MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+			MPI_Recv(buffer, buff_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			cout << "[" << rank << "]: " << "Received: " << ((status.MPI_TAG==ANSWER_ID)?"[ANSWER]":"[ERROR]")  << " from [" << status.MPI_SOURCE << "]" << endl;
+			if (status.MPI_TAG==ANSWER_ID)
 			{
-				MPI_Status status;
-				MPI_Recv(buffer, buff_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-				cout << "[" << rank << "]: " << "Received: " << status.MPI_TAG  << " from [" << status.MPI_SOURCE << "]" << endl;
-				switch (status.MPI_TAG)
-				{
-					case ANSWER_ID:
-						cout << "\t\tEverything is OK!" << endl;
-						break;
-					case ERROR_ID:
-						cout << "\t\tError!!! Start voting!" << endl;
-						break;
-				}
+				cout << ">" << status.MPI_SOURCE << "<: OK" << endl;
+			} else {
+				cout << ">" << status.MPI_SOURCE << "<: Failed! Start voting!" << endl;
+				voting = true;
 			}
 		}
 	}
+
+	for (int chosenRank=0; chosenRank<size-1; chosenRank++)
+	{
+		if (rank==chosenRank) {
+			if (voting)
+			{
+				for (int i=chosenRank+1; i<size; i++)
+				{
+					MPI_Send(buffer, buff_size, MPI_INT, i, VOTING_ID, MPI_COMM_WORLD);
+					cout << "[" << rank << "]: " << "Sending [VOTING] to [" << i << "]" << endl;
+				}
+			}
+			Sleep(400);
+		} else {
+			if (rank > chosenRank)
+			{
+				Sleep(400);
+				MPI_Status status;
+				MPI_Probe(chosenRank, MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+				MPI_Recv(buffer, buff_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+				cout << "[" << rank << "]: " << "Received: " << ((status.MPI_TAG==VOTING_ID)?"[VOTING]":"[???]")  << " from [" << status.MPI_SOURCE << "]" << endl;
+				Sleep(400);
+				if (rank==size-1)
+				{
+					MPI_Send(buffer, buff_size, MPI_INT, status.MPI_SOURCE, ERROR_ID, MPI_COMM_WORLD);
+					cout << "[" << rank << "]: " << "Sending [ERROR] to [" << status.MPI_SOURCE << "]" << endl;
+				} else {
+					MPI_Send(buffer, buff_size, MPI_INT, status.MPI_SOURCE, VOTING_OK_ID, MPI_COMM_WORLD);
+					cout << "[" << rank << "]: " << "Sending [VOTING_OK] to [" << status.MPI_SOURCE << "]" << endl;
+				}
+				Sleep(400);
+			}
+		}
+	}
+	Sleep(1000);
+	for (int chosenRank=0; chosenRank<size-1; chosenRank++)
+	{
+		if (rank==chosenRank) {
+			if (voting)
+			{
+				int votes=0;
+				for (int i=chosenRank+1; i<size; i++)
+				{
+					MPI_Status status;
+					MPI_Probe(i, MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+					MPI_Recv(buffer, buff_size, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+					cout << "[" << rank << "]: " << "Received: " << ((status.MPI_TAG==VOTING_OK_ID)?"[VOTING_OK]":"[ERROR]")  << " from [" << status.MPI_SOURCE << "]" << endl;
+					if (status.MPI_TAG==VOTING_OK_ID)
+					{
+						votes++;
+					}
+				}
+				if (votes>0)
+				{
+					cout << "[" << rank << "]: I'm not coordinator!" << endl;
+				} else {
+					coordinating = true;
+					cout << "[" << rank << "]: I'm new coordinator!" << endl << "[End of voting]" << endl;
+				}
+			}
+			Sleep(400);
+		}
+	}	
 }
 
 void main(int argc, char **argv)
